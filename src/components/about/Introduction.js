5d0bf2a3d05102ae27cbfb39359c62d773dec3a3
Ibad <ibad23ahmad@gmail.com>
2021-05-19 10:29:57 +0500

    updating profile  picture component

--- src/components/about/Introduction.js
+++ src/components/about/Introduction.js
@@ -1,16 +1,9 @@
 import React from "react";
-import profile_img from "../../assets/profile_picture.jpeg";
 
 export default function Introduction() {
   return (
     <div id="profile-container">
-      <div id="profile-image">
-        <img
-          // src={profile_img}
-          // alt="profile_photo"
-          style={{ objectFit: "contain", backgroundSize: "100% 100%" }}
-        />
-      </div>
+      <div id="profile-image"></div>
       <div id="profile-introduction">
         <h5 id="who-this">Who's this guy</h5>
         <p id="who-this-description">
