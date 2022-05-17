33ef123a4c8a14789ecd0c61bfc0a80772bbadaa
Ibad Ahmad <ibad23ahmad@gmail.com>
2022-05-17 13:12:03 +0500

    Add files via upload

---
+++ src/components/projects/ProjectsData.js
@@ -0,0 +1,188 @@
+import travelguide2 from "../../assets/travelguide.jfif";
+import trimax from "../../assets/trimax.jpg";
+import instagram2 from "../../assets/instagram.jpg";
+import blogging2 from "../../assets/blogging.jfif";
+import portfolio2 from "../../assets/portfolio.jpg";
+import gym2 from "../../assets/gym.jpg";
+import travelguide3 from "../../assets/travelguide/Capture-1.PNG";
+import travelguide4 from "../../assets/travelguide/Capture-2.PNG";
+import travelguide5 from "../../assets/travelguide/Capture-3.PNG";
+import travelguide6 from "../../assets/travelguide/Capture-4.PNG";
+import travelguide7 from "../../assets/travelguide/Capture-5.PNG";
+import gym3 from "../../assets/gym/Capture-1.PNG";
+import gym4 from "../../assets/gym/Capture-2.PNG";
+import gym5 from "../../assets/gym/Capture-3.PNG";
+import gym6 from "../../assets/gym/Capture-4.PNG";
+import gym7 from "../../assets/gym/Capture-5.PNG";
+import ntt1 from "../../assets/ntt1.jpeg";
+import ntt2 from "../../assets/ntt/ntt-2.png";
+import ntt3 from "../../assets/ntt/ntt-3.png";
+import ntt4 from "../../assets/ntt/ntt-4.png";
+import ntt5 from "../../assets/ntt/ntt-5.png";
+import quiz2 from "../../assets/quiz/quiz-2.png";
+import quiz3 from "../../assets/quiz/quiz-3.png";
+import quiz4 from "../../assets/quiz/quiz-4.png";
+import quiz5 from "../../assets/quiz/quiz-5.png";
+import soulz2 from "../../assets/soulz/soulz-2.png";
+import soulz3 from "../../assets/soulz/soulz-3.png";
+import soulz4 from "../../assets/soulz/soulz-4.png";
+import soulz5 from "../../assets/soulz/soulz-5.png";
+import warbls2 from "../../assets/warbls/warbls-2.png";
+import warbls3 from "../../assets/warbls/warbls-3.png";
+import warbls4 from "../../assets/warbls/warbls-4.png";
+import instagram3 from "../../assets/instagram/Capture-1.PNG";
+import instagram4 from "../../assets/instagram/Capture-2.PNG";
+import instagram5 from "../../assets/instagram/Capture-3.PNG";
+import blogging3 from "../../assets/blogging/Capture-1.PNG";
+import blogging4 from "../../assets/blogging/Capture-2.png";
+import blogging5 from "../../assets/blogging/Capture-3.PNG";
+import worko3 from "../../assets/worko/Capture-1.PNG";
+import worko4 from "../../assets/worko/Capture-2.PNG";
+import worko5 from "../../assets/worko/Capture-3.PNG";
+import portfolio3 from "../../assets/portfolio/Capture-1.PNG";
+import portfolio4 from "../../assets/portfolio/Capture-2.PNG";
+import portfolio5 from "../../assets/portfolio/Capture-3.PNG";
+
+export const data = [
+  {
+    name: "NTT Experience",
+    images: [ntt2, ntt3, ntt4, ntt5],
+    image2: ntt1,
+    technologies: " React / Globkit / Mapbox",
+    category: ["React"],
+    github: "https://github.com/aliahmad11403/Ntt-experience",
+    deployed: "https://ntt-experience.netlify.app/",
+    description:
+      "A realtime data center based map experineced created with Globkit jsm and Mapbox platform.",
+    month_created: "November 2021",
+    role: "Frontend",
+  },
+  {
+    name: "Order AI",
+    images: [quiz2, quiz3, quiz4, quiz5],
+    image2:
+      "https://images.unsplash.com/photo-1509315703195-529879416a7d?utm_medium=medium&w=700&q=50&auto=format",
+    technologies: " React / SASS / NodeJS",
+    category: ["ReactJS", "| ", "NodeJS", "| ", "SASS"],
+    github: "https://github.com/ibadahmad12/Order-ai-quiz",
+    deployed: "https://order-ai-quiz.netlify.app/",
+    description:
+      "Travel guiding web app that contain sections like blogs,tour packages and tour best offers. All data is hooked up with NoSQL database MongoDB. APIS are designed with GraphQL and Apollo client is used as a GraphQL client for ReactJS. No UI library is used throughout just the pure CSS. For Carousel purposes, Libraries like react slick, swipeJS are given preferences,react reposive carousel. For blogs, GraphQL subscription type has been used to provide real-time features for user added comments on individual blogs.Backend (NodeJS)is hosted on heorku and ReactJS is hosted with netlify.",
+    month_created: "Feburary 2022",
+    role: "Full Stack",
+  },
+  {
+    name: "Warbls",
+    images: [warbls2, warbls3, warbls4],
+    image2: "https://cdn-images-1.medium.com/max/1600/0*uWq2EZFrUEX0qUhZ",
+    technologies: " React / Nodejs / Redux",
+    category: ["React", "| ", "Redux", "| ", "NodeJS"],
+    github: "https://github.com/faheemsajjad65/warbls",
+    deployed: "https://warbls.com/",
+    description:
+      "Travel guiding web app that contain sections like blogs,tour packages and tour best offers. All data is hooked up with NoSQL database MongoDB. APIS are designed with GraphQL and Apollo client is used as a GraphQL client for ReactJS. No UI library is used throughout just the pure CSS. For Carousel purposes, Libraries like react slick, swipeJS and react reposive carousel are given preferences. For blogs, GraphQL subscription has been used to provide real-time features for user added comments on individual blogs.Backend (NodeJS)is hosted on heorku and ReactJS is hosted with netlify.",
+    month_created: "October 2021",
+    role: "Full Stack",
+  },
+  {
+    name: "Soulz",
+    images: [soulz2, soulz3, soulz4, soulz5],
+    image2:
+      "https://images.unsplash.com/photo-1642525027649-00d7397a6d4a?utm_medium=medium&w=700&q=50&auto=format",
+    technologies: " React / SASS",
+    category: ["React", "| ", "SASS"],
+    github: "https://github.com/ibadahmad12/Soulz",
+    deployed: "https://soulz.netlify.app/",
+    description:
+      "Travel guiding web app that contain sections like blogs,tour packages and tour best offers. All data is hooked up with NoSQL database MongoDB. APIS are designed with GraphQL and Apollo client is used as a GraphQL client for ReactJS. No UI library is used throughout just the pure CSS. For Carousel purposes, Libraries like react slick, swipeJS are given preferences,react reposive carousel. For blogs, GraphQL subscription type has been used to provide real-time features for user added comments on individual blogs.Backend (NodeJS)is hosted on heorku and ReactJS is hosted with netlify.",
+    month_created: "April 2021",
+    role: "Frontend",
+  },
+
+  {
+    name: "Travel Guide",
+    images: [
+      travelguide3,
+      travelguide4,
+      travelguide5,
+      travelguide6,
+      travelguide7,
+    ],
+    image2: travelguide2,
+    technologies: " React / GraphQL / Apollo-Client ",
+    category: ["React", "| ", "NodeJS"],
+    github: "https://github.com/IbadAhmad80/Brando-TravelGuide",
+    deployed: "https://brando-travelguide.netlify.app/",
+    description:
+      "Travel guiding web app that contain sections like blogs,tour packages and tour best offers. All data is hooked up with NoSQL database MongoDB. APIS are designed with GraphQL and Apollo client is used as a GraphQL client for ReactJS. No UI library is used throughout just the pure CSS. For Carousel purposes, Libraries like react slick, swipeJS are given preferences,react reposive carousel. For blogs, GraphQL subscription type has been used to provide real-time features for user added comments on individual blogs.Backend (NodeJS)is hosted on heorku and ReactJS is hosted with netlify.",
+    month_created: "August 2021",
+    role: "Full Stack",
+  },
+
+  {
+    name: "Buisniss Agency",
+    images: [worko3, worko4, worko5],
+    image2: trimax,
+    technologies: " React / Bootstrap 5.0 ",
+    category: ["React"],
+    github: "https://github.com/IbadAhmad80/Worko",
+    deployed: "https://worko.netlify.app/",
+    description:
+      "Its a bootstrap template designed with React-Bootstrap V5. Its more of an portfolio website for digital agency that contains Bootstrap components like cards,navbar,footer,images etc. Application is perfectly responsive throughout. There is no dynamic content lying in there since its just a landing page with static features. Tells you about the history , projects and team of an agency.You can find projects in the form of image gallery. This template is hosted on internet with netlify.",
+    month_created: "August 2021",
+    role: "Frond End",
+  },
+  // {
+  //   name: "GymHut - Not Responsive",
+  //   images: [gym3, gym4, gym5, gym6, gym7],
+  //   image2: gym2,
+  //   category: ["React", "Redux", "NodeJS"],
+  //   technologies: " React / Node ",
+  //   github: "https://github.com/IbadAhmad80/GymHut",
+  //   deployed: "https://gymhut.netlify.app/",
+  //   description:
+  //     "Gymhut is a clean, modern Gym Website. it is designed for Sport, Gym, Fitness, Aerobic, Yoga,Boxing, Trainer, Coach, businesses etc. All the Blogs,packages section are dynamically managed with mongoDB and nodejs.Authentication process is manages with jwt while mantaining the state managment with reux.Moreover there is also an extra added e-commerce feature where you can buy gym mrchandise that is hooked up with stripe payment.web app is hosted on heroku (expressJS) and netlify (ReactJS).",
+  //   month_created: "December 2020",
+  //   role: "Full Stack",
+  // },
+  // {
+  //   name: "Instagram Clone - Not Responsive",
+  //   images: [instagram3, instagram4, instagram5],
+  //   image2: instagram2,
+  //   technologies: " React / Firebase ",
+  //   category: ["React", "SASS"],
+  //   github: "https://github.com/IbadAhmad80/Instagarm-Clone",
+  //   deployed: "https://instagarm-clone-5b04e.web.app/",
+  //   description:
+  //     "Instagram clone that contais basic functionalities of social media app like post creation , following other members , liking their post, seeing thier feed  and commenting on their posts respectively. Firebase is used with react as datastore, authentication and authorization service and media storage with thier services like firstore,firebase authentication and firebase storage. CSS Preprocessor SASS is used for design purpose. Framer Motion has been used in order to provide better animation experience with animation. this website is hosted on firebase servers.",
+  //   month_created: "Feburary 2021",
+  //   role: "Full Stack",
+  // },
+  {
+    name: "Portfolio",
+    images: [portfolio3, portfolio4, portfolio5],
+    image2: portfolio2,
+    technologies: " React / SASS ",
+    category: ["React", "| ", "SASS", "| ", "NodeJS"],
+    github: "https://github.com/IbadAhmad80/Portfolio",
+    deployed: "https://findibad.netlify.app/",
+    description:
+      "This is my personal portfolio website which are on right now. It showcases my carrer growth as a self learning full-satck developer in the form of project and some of the open source contributions so far. Animate on Scroll library is been used for animation purpose throughout. App contains sections like skill-set i require in this domain with the image gallery for projets section and my contact information in the lower part of the app.Website is live with netlify.",
+    month_created: "May 2021",
+    role: "Front End",
+  },
+
+  {
+    name: "Blogging Website",
+    images: [blogging3, blogging4, blogging5],
+    image2: blogging2,
+    technologies: " NextJS / Strapi ",
+    category: ["React", " |", "SASS", " |", "Redux"],
+    github: "https://github.com/IbadAhmad80/Blogging-Web_App",
+    deployed: "",
+    description:
+      "Blogging app compiled with NextJS for better server-side experience in the shape of reactJS. Implemented with Strapi CMS, contains blogs from different categories. Comments functionality is beed added on each blog. Authenticated users have an option to create a blogg too. You can become a fiture member by subscription method which is hooked up with Stripe payment gateway.At last the account section part is handled with redux js and local storage. This app is not live on internet yet, hoepfully will be in near future.",
+    month_created: "March 2021",
+    role: "Full Stack",
+  },
+];
